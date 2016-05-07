0de46b765cbeb86d3a595bf4ec6d3772699d647d
Tee Blagdon <tjlay23@gmail.com>
2016-05-06 19:24:24 -0700

    Solve sumBelow prompt

--- src/recursion.js
+++ src/recursion.js
@@ -31,10 +31,7 @@ var isEven = function(n) {
 // sumBelow(10); // 45
 // sumBelow(7); // 21
 var sumBelow = function(n) {
-  if (n === 0) {
-    return 0;
-  }
-  return n < 0 ? (n+1) + sumBelow(n+1) : (n-1) + sumBelow(n-1);
+  return n === 0 ? 0 : n < 0 ? (n+1) + sumBelow(n+1) : (n-1) + sumBelow(n-1);
 };
 
 // 6. Get the integers in range (x, y).
